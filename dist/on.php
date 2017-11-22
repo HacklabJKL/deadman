0616702dd017cfa6a39ffc114c74bc97a88570a1
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2017-11-22 22:19:38 +0200

    Add power on API

---
+++ dist/on.php
@@ -0,0 +1,22 @@
+<?php
+require_once(__DIR__.'/../lib/common.php');
+
+$status = relay_status(3);
+
+if ($status === "off") {
+    // Initial power-up allows only 2 minutes of power. Dead man's
+    // switch must be pressed after that to get the maximum of 5
+    // minutes.
+    relay_set_timeout(3, "2min");
+    `sispmctl -o3`; // Turn on relay
+    $new_status = relay_status(3);
+    $out = relay_left(3);
+
+    if ($new_status !== "on" || $out === FALSE) {
+        $out = erray("Unable to turn on, try again!");
+    }
+} else {
+    $out = erray("Relay already on");
+}
+
+jprint($out);
