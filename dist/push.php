6a2762dffc3345027179986fe3d9c8fe15c57209
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2017-11-22 21:27:22 +0200

    Return error if power went down before entering a valid code

--- dist/push.php
+++ dist/push.php
@@ -9,12 +9,23 @@ if (!preg_match('/^[0-9]{6}$/', $totp)) {
         "error" => "Illegal input",
     ];
 } else if (`oathtool -w 1 --totp deadabbabeef $totp` === NULL) {
-    $out =  [
+    $out = [
         "error" => "Incorrect code",
     ];
 } else {
     `sispmctl -A3 --Aat "\`date '+%F %H:%M' -d 5min\`" --Ado off`;
     $out = relay_left(3);
+
+    $status = relay_status(3);
+    if ($out === NULL || $status === NULL) {
+        $out = [
+            "error" => "Relay control failed, try again!",
+        ];
+    } else if ($status === "off") {
+        $out = [
+            "error" => "Power already down",
+        ];
+    }
 }
 
 print(json_encode($out)."\n");
--- lib/common.php
+++ lib/common.php
@@ -1,5 +1,11 @@
 <?php
 
+// FIXME $relay_id in these functions allow shell injection. Never
+// call these with user provided data
+
+/**
+ * Queries time left for the given relay state change
+ */
 function relay_left($relay_id) {
     $ret = preg_match('/"([^"]*)"/', `sispmctl -a $relay_id`, $matches);
 
@@ -12,4 +18,16 @@ function relay_left($relay_id) {
         "left" => $timeout->getTimestamp() - time(),
         "timeout" => $timeout->getTimestamp(),
     ];
+}
+
+/**
+ * Queries relay status. Returns "on", "off" or FALSE in case of
+ * failure.
+ */
+function relay_status($relay_id) {
+    $ret = preg_match('/:\t(off|on)/', `sispmctl -g $relay_id`, $matches);
+    if ($ret === FALSE || !array_key_exists(1, $matches)) {
+        return FALSE;
+    }
+    return $matches[1];
 }
